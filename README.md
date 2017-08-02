1a07010b41c1d8937f5be01553407f2102c78be8
Donald Fox <dfox682000@gmail.com>
2017-08-01 21:18:49 -0500

    Updated Read Me file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Bamazon
+
+Bamazon is a data driven js node command line application with a MySQL database backend that displays products for sale in a table.  
+
+After launching the app the user is shown the table of items and  prompted to enter the name of the product they wish to purchase.
+
+Then the user is asked how many units of the product they would like to buy.
+
+Once the customer has placed the order, the application checks if the store has enough of the product to meet the customer's request.
+
+If not, the app logs "Not a Valid choice!!, and prevents the order from going through.
+
+See attached screenshots.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,68 +13,68 @@ var connection = mysql.createConnection({
   password: "",
   database: "bamazon_DB"
 });
-connection.connect(function (err) {
-    if (err) {
-        console.log("error connnecting: " + err.stack);
-    }
-    makeTable();
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connnecting: " + err.stack);
+  }
+  makeTable();
 });
 // function connects to database and displays table
-var makeTable = function () {
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw error;
-       var table = new Table({
-    head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
-  });
-  for (var i = 0; i < res.length; i++) {
-    table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
-  }
-  console.log(table.toString());
-        promptCustomer(res);
+var makeTable = function() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw error;
+    var table = new Table({
+      head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
     });
+    for (var i = 0; i < res.length; i++) {
+      table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+    console.log(table.toString());
+    promptCustomer(res);
+  });
 };
 // function prompts the customer & depletes stock if something is purchased
-var promptCustomer = function (res) {
-    inquire.prompt([{
-        type: 'input',
-        name: 'choice',
-        message: ' What would you like to purchase? [Quit with Q]'
-    }]).then(function (val) {
-        var correct = false;
-        for (var i = 0; i < res.length; i++) {
-            if (res[i].product_name === val.choice) {
-                correct = true;
-                var product = val.choice;
-                var id = i;
-                inquire.prompt([{
-                    type: 'input',
-                    name: 'quant',
-                    message: 'How many would you like to buy?'
-                }]).then(function (val) {
-                    if ((res[id].stock_quantity - val.quant) > 0) {
-                        connection.query(
-                            "UPDATE products SET stock_quantity='" + (res[id].stock_quantity - val.quant) +
-                            "' WHERE product_name='" + product + "'",
-                            function (err, res2) {
-                                if (err) {
-                                    throw err;
-                                }
-                                console.log("Purchase successful.");
-                                makeTable();
-                            });
-                    } else {
-                        console.log("Not a Valid choice!!");
-                        promptCustomer(res);
-                    }
-                });
-            }
-            if (val.choice == "Q" || val.choice == "q") {
-                process.exit();
-            }
-        }
-        if (i === res.length && correct === false) {
-            console.log("Not a valid selection");
+var promptCustomer = function(res) {
+  inquire.prompt([{
+    type: 'input',
+    name: 'choice',
+    message: ' What would you like to purchase? [Quit with Q]'
+  }]).then(function(val) {
+    var correct = false;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].product_name === val.choice) {
+        correct = true;
+        var product = val.choice;
+        var id = i;
+        inquire.prompt([{
+          type: 'input',
+          name: 'quant',
+          message: 'How many would you like to buy?'
+        }]).then(function(val) {
+          if ((res[id].stock_quantity - val.quant) > 0) {
+            connection.query(
+              "UPDATE products SET stock_quantity='" + (res[id].stock_quantity - val.quant) +
+              "' WHERE product_name='" + product + "'",
+              function(err, res2) {
+                if (err) {
+                  throw err;
+                }
+                console.log("Purchase successful.");
+                makeTable();
+              });
+          } else {
+            console.log("Not a Valid choice!!");
             promptCustomer(res);
-        }
-    });
+          }
+        });
+      }
+      if (val.choice == "Q" || val.choice == "q") {
+        process.exit();
+      }
+    }
+    if (i === res.length && correct === false) {
+      console.log("Not a valid selection");
+      promptCustomer(res);
+    }
+  });
 }
